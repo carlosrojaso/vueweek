45c42bf669c3063f83ee68be05130508eb72ee67
Oscar Granada <oscar.granada@autodesk.com>
2019-02-10 12:18:27 -0500

    Added banner component

---
+++ src/components/Banner/Banner.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="Banner">
+    <div class="Banner-container">
+      <span class="Banner-date">20 de Abr - 28 de Abr. Globant Medellín - Colombia</span>
+      <h2 class="Banner-title">
+        Developer Week: <img class="Banner-logo" src="../../assets/logo.png" alt="logo" /> Vue
+      </h2>
+      <a class="Banner-streamingLink" href="http://youtube.com" target="_blank">Livestream</a>
+    </div>
+  </section>
+</template>
+
+<script src="./Banner.js"></script>
+
+<style lang="scss" src="./Banner.scss"></style>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="Home">
-  </div>
+  <main class="Home">
+    <Banner />
+  </main>
 </template>
 
 <script>
 // @ is an alias to /src
+import Banner from "@/components/Banner/Banner.vue";
 
 export default {
   name: 'home',
   components: {
+    Banner
   }
 }
 </script>
