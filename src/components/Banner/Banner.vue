841847ad9ac57ba27557ade153869bef4203ed5f
Oscar Granada <oscar.granada@autodesk.com>
2019-02-11 11:35:25 -0500

    Fixed to accomplish with BEM convention.

--- src/components/Banner/Banner.vue
+++ src/components/Banner/Banner.vue
@@ -1,11 +1,11 @@
 <template>
   <section class="Banner">
-    <div class="Banner-container">
-      <span class="Banner-date">20 de Abr - 28 de Abr. Globant Medellín - Colombia</span>
-      <h2 class="Banner-title">
-        Developer Week: <img class="Banner-logo" src="../../assets/logo.png" alt="logo" /> Vue
+    <div class="Banner__container">
+      <span class="Banner__date">20 de Abr - 28 de Abr. Globant Medellín - Colombia</span>
+      <h2 class="Banner__title">
+        Developer Week: <img class="Banner__logo" src="../../assets/logo.png" alt="logo" /> Vue
       </h2>
-      <a class="Banner-streamingLink" href="http://youtube.com" target="_blank">Livestream</a>
+      <a class="Banner__streamingLink" href="http://youtube.com" target="_blank">Livestream</a>
     </div>
   </section>
 </template>
